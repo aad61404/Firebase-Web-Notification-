441e218213889f7f91248ac89b1a9e5b0fe600fc
aad61404 <asd61404@gmail.com>
2020-06-12 17:47:10 +0800

    public 裡面為檔案 外部都是測試檔

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,41 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-messaging.js');
+importScripts('/__/firebase/3.8.0/firebase-app.js');
+importScripts('/__/firebase/3.8.0/firebase-messaging.js');
 importScripts('/__/firebase/init.js');
 
-firebase.initializeApp({
-    messagingSenderId: "726401871837"
-})
-
-const initMessaging = firebase.message();
-// var firebaseConfig = {
-//     apiKey: "AIzaSyDYDCcag_fnrz6u1fhOlDUmnyiPgHrJrJs",
-//     authDomain: "web-notification-61213.firebaseapp.com",
-//     databaseURL: "https://web-notification-61213.firebaseio.com",
-//     projectId: "web-notification-61213",
-//     storageBucket: "web-notification-61213.appspot.com",
-//     messagingSenderId: "726401871837",
-//     appId: "1:726401871837:web:00ecf8243f64ecd205d034",
-//     measurementId: "G-PKKFBCVR87"
-// };
-
-// firebase.initializeApp(firebaseConfig);
-
-// const messaging = firebase.messaging();
-// messaging.usePublicVapidKey("BPPNEWamSL4yvTZt07V7I_3BAK8qnDgAWdoj5r_7ukRi9reBBtSajGJdbMqqnk9MWKD52Z7RBvQYOgHP9f2IbEA");
-
-// messaging.requestPermission()
-// .then(function(){
-//     console.log('Have Permission')
-//     return messaging.getToken();
-// })
-// .then(function(token){
-//     console.log('------------get Token--------------')
-//     console.log(token);
-// })
-// .catch(function(err){
-//     console.log(err);
-// })
-
-// messaging.onMessage(function(payload){
-//     console.log('onMessage:' , payload);
-// })
+firebase.messaging();
